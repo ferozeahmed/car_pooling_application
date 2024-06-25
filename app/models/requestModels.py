1eb693e7e726396849387105d2f32f98e138800e
Feroze Ahmed R <ferozebtech2007@gmail.com>
2024-06-25 23:22:31 +0530

    Add files via upload

--- app/models/requestModels.py
+++ app/models/requestModels.py
@@ -1,1 +1,33 @@
+from pydantic import BaseModel
+from typing import List
 
+
+# Create model for vehicle request
+class Vehicle(BaseModel):
+    type: str
+    seats_offered: str
+
+
+# Create model for users request
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    mail_id: str
+    mobile: str
+    address: str
+    latitude: float
+    longitude: float
+    vehicle: List[Vehicle]
+
+
+# Create model for ride request
+class Ride(BaseModel):
+    mail_id: str
+    latitude: float
+    longitude: float
+    destination: str
+    seats_offered: int
+    riders: List[str]
+    date: str
+    status: str
+    vehicle: str
